8412cddcf61e3c514850bf62e8172ddaba1e1e0c
Atqarana <atqarana@gmail.com>
2024-08-19 00:28:16 +0500

    V2

--- models/playht.js
+++ models/playht.js
@@ -36,10 +36,10 @@ async function play(text) {
     const streamingOptions = {
         // must use turbo for the best latency
         voiceEngine: "PlayHT2.0-turbo",
-        id: 'kid',
+       // id: 'kid',
         // this voice id can be one of our prebuilt voices or your own voice clone id, refer to the`listVoices()` method for a list of supported voices.
         voiceId:
-           "s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json",
+           "s3://voice-cloning-zero-shot/88f357c9-90a8-433b-ae23-966ccd3ade9c/original/manifest.json",
         // you can pass any value between 8000 and 48000, 24000 is default
         sampleRate: 44100,
         // the generated audio encoding, supports 'raw' | 'mp3' | 'wav' | 'ogg' | 'flac' | 'mulaw'
